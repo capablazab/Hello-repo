a6026df58c57e405d0d76e137ea07e9834c103cb
Carlos Apablaza <carlosapablazab@gmail.com>
2011-09-03 00:55:42 -0300

    third commit

---
+++ f_pointers.c
@@ -0,0 +1,55 @@
+/*
+ * Differences between passing arguments by value (no pointers) and
+ * passing them by reference (through pointers)
+ * 
+ * Two funcions are given, for the two cases.
+ * 
+ */
+
+#include <stdio.h>
+
+void main()
+{
+	int var=0, result;
+	int *p;
+	
+	// case 1: without pointers
+	result=Add3(var);
+
+	printf("Result: %d\n", result);
+	
+	// case 2: with pointers
+	
+	p=&var;
+	
+	Add3_p(p);
+	
+	printf("var: %d\n", var);
+	
+	
+}
+
+/*
+ * 1. without pointers
+ * 
+ * Adds 3 units to the variable num
+ * Returns: int num
+ */
+int Add3(int num)
+{
+	num+=3;
+	
+	return num;
+}
+
+/*
+ * 2. with pointers
+ * 
+ * Adds 3 units to the variable referenced by p.
+ * Returns: Nothing
+ */
+void Add3_p(int *p)
+{
+	*p+=3;
+}
+
